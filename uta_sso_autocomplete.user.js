acb4f5271dd8e24136be5c150fa7a7ea475c44b3
Mace Ojala <mace@kirjakaapeli.lib.hel.fi>
2013-09-30 00:29:38 +0300

    Added update URL, directly from Github

--- uta_sso_autocomplete.user.js
+++ uta_sso_autocomplete.user.js
@@ -2,7 +2,8 @@
 // @name        UTA SSO autocomplete enabler
 // @namespace   https://xmacex.wordpress.com/userscript/
 // @include     https://sso.uta.fi/cas/login*
-// @version     1
+// @version     1.1
+// @updateURL   https://github.com/xmacex/uta-sso-autocomplete/raw/master/uta_sso_autocomplete.user.js
 // @icon        http://www.uta.fi/favicon.ico
 // @grant       none
 // ==/UserScript==
