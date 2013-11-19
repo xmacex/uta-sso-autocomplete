dd501afe020659c118351a9525e974d928121498
Mace Ojala <mace@kirjakaapeli.lib.hel.fi>
2013-11-19 14:40:49 +0200

    Automate pressing the "Log in" button on the UTAPAC campus wifi

--- utapac_autocomplete.user.js
+++ utapac_autocomplete.user.js
@@ -2,7 +2,7 @@
 // @name        UTAPAC wifi login autocomplete enabler
 // @namespace   https://xmacex.wordpress.com/userscript/
 // @include     https://utapacs.uta.fi/tino.cgi*
-// @version     1.0
+// @version     1.1
 // @icon        http://www.uta.fi/favicon.ico
 // @grant       none
 // ==/UserScript==
@@ -13,3 +13,11 @@ if(form.getAttribute("autocomplete") == "off") {
     form.removeAttribute("autocomplete")
     console.log("autocomplete was disabled by the service provider, but was forced enabled.")
 }
+
+// push the login button
+// $(window).load(function(e) {
+window.onload = (function(e) {
+    console.log("pressing the login button now")
+    document.getElementsByName('login')[0].click()
+});
+
